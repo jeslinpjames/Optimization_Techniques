cd61f29e3f979e62f977abc5fccc6b3c243c969c
jeslinpjames <jeslinpjames007@gmail.com>
2024-01-23 15:22:27 +0530

    0-1 knapsack complete

--- 0-1Knapsack.py
+++ 0-1Knapsack.py
@@ -1,10 +1,22 @@
-
+def knapSack(C, W, m, n):
+    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    for i in range(n+1):
+        for j in range(m+1):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            elif W[i-1]<=j:
+                dp[i][j]=max(C[i - 1] + dp[i - 1][j- W[i - 1]], dp[i - 1][j])
+            else:
+                dp[i][j] = dp[i - 1][j]
+    return dp[n][m]
+    
 if __name__=='__main__':
     m = int(input("Enter the capacity: "))
     n = int(input("Enter the number of items : "))
     C = []
     W = []
     for i in range(n):
-        C[i]=int(input("Enter the cost of item ",i, " : "))
-        W[i]=int(input("Enter the weight of item ",i, " : "))
-    KnapSack(C,W,m,n)
+        C.append(int(input("Enter the cost of item {}: ".format(i + 1))))
+        W.append(int(input("Enter the weight of item {}: ".format(i + 1))))
+    result = knapSack(C,W,m,n)
+    print("The maximum value that can be obtained is:", result)
